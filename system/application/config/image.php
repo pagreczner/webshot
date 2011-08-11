b92860201a3ce44dcc819561b3e1c9ccba848556
Alicia <alicia.x.tang@gmail.com>
2011-08-11 12:42:17 -0700

    Use relative path instead

--- system/application/config/image.php
+++ system/application/config/image.php
@@ -5,7 +5,7 @@
 | Image Directory
 |--------------------------------------------------------------------------
 */
-$config['image_directory'] = '/home/imageq/images';
+$config['image_directory'] = BASEPATH.'/../images';
 
 $config['default_full_image'] = 'default_full.jpg';
 
